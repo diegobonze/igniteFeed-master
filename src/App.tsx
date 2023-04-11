c4c03d001f6999c2ae981e95b2837fb1b9ee9ce9
Diego Bonze <diegobonze747@gmail.com>
2023-04-11 20:41:32 -0300

    finished commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Header } from "./components/Header";
+import { Post, PostType } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://github.com/diegobonze.png",
+      name: "Diego Bonze",
+      role: "Web developer Júnior: Linear",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz na trilha Ignite da Rocketseat. O nome do projeto é IgniteFeed 🚀",
+      },
+
+      { type: "link", content: "👉 Github/IgniteFeed" },
+    ],
+    publishedAt: new Date("2023-03-13 15:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/blenybonze.png",
+      name: "Bleny Bonze",
+      role: "Web developer Sênior: Linear",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz na trilha Ignite da Rocketseat. O nome do projeto é IgniteFeed 🚀",
+      },
+
+      { type: "link", content: "👉 Github/IgniteFeed" },
+    ],
+    publishedAt: new Date("2023-03-12 14:00:00"),
+  },
+];
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post 
+                key={post.id}
+                post={post}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,63 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/diegobonze.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Bonze</strong>
+              <time
+                title="09 de Março de às 20:21"
+                dateTime="2023-03-09 20:20:28"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
